7c6dd82af6330b18078ce4b7502f9e29b45850e7
URIEL JOAQUIN GALANTI <49553172@ort>
2025-05-08 15:40:19 -0300

    Trabajando en Index

--- Models/Ahorcado.cs
+++ Models/Ahorcado.cs
@@ -1,9 +1,9 @@
 namespace TP04;
 public static class Ahorcado{
-    public static string palabra {public set; get;}
+    public static string palabra {public set; get;} 
     public static List<char> letras {public set; get;}
-
     public static void inicializarAhorcado(){
+        palabra = "Florencia";
         letras.Clear();
         foreach (char i in palabra){
                     letras.add(i);
