ec785d2220276bdb8e684aa40828e122118e813e
URIEL JOAQUIN GALANTI <49553172@ort>
2025-05-08 15:08:09 -0300

    Cree la clase Ahorcado y el método para inicializarlo

---
+++ Models/Ahorcado.cs
@@ -0,0 +1,12 @@
+namespace TP04;
+public static class Ahorcado{
+    public static string palabra {public set; get;}
+    public static List<char> letras {public set; get;}
+
+    public static void inicializarAhorcado(){
+        letras.Clear();
+        foreach (char i in palabra){
+                    letras.add(i);
+        }
+    }
+}
